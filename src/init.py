62e58f541b8e3f7fdb4ed8d4d0cb07d74c902189
RmValn <valik8355@gmail.com>
2024-12-22 21:11:35 +0200

    Add __init__.py to servers_interact for package recognition

--- src/init.py
+++ src/init.py
@@ -4,7 +4,7 @@ from .servers_interact.local import Server
 from .servers_interact.remote import Remote
 from .logs import Log
 from .pin_state import send_inputs
-from ota import OTA
+from .ota import OTA
 class Init:
     def __init__(self,ssid,password,server_ip):
         connectWiFi(ssid, password)
