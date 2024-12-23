04a82727f51d58ba23662513cbcb867abf8b3640
RmValn <valik8355@gmail.com>
2024-12-23 20:49:36 +0200

    modified:   src/servers_interact/local.py

--- src/servers_interact/local.py
+++ src/servers_interact/local.py
@@ -6,7 +6,7 @@ class Router:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls._instance = super().__new__(cls)
             cls._instance.routes = {}
         return cls._instance
     
