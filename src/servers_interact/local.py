54716f493418952e43ba23991a255a68905f4305
RmValn <valik8355@gmail.com>
2024-12-23 21:25:29 +0200

    modified:   src/servers_interact/local.py

--- src/servers_interact/local.py
+++ src/servers_interact/local.py
@@ -5,6 +5,7 @@ class Router:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
+        print('курва сука блять')
         if not cls._instance:
             cls._instance = super().__new__(cls)
             cls._instance.routes = {}
