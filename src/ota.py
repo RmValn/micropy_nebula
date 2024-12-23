a504f0dca58189fe10a1f991e859150199559a19
RmValn <valik8355@gmail.com>
2024-12-23 13:11:00 +0200

    t

--- src/ota.py
+++ src/ota.py
@@ -14,7 +14,7 @@ class OTA:
         try:
             print("Checking update...")
             await Log.send_log("Checking update...")
-            response = Remote.send_request('version')
+            response = await Remote.send_request('version')
             if response.status_code == 200:
                 server_version = response.json().get("version")
                 if server_version != CURRENT_VERSION:
